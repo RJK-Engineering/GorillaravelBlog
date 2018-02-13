311b4ae2dd6cc91f8dc1eb6b827850c1516561e2
heinerbehrends <heinerbehrends@hotmail.com>
2018-02-13 12:12:21 +0100

    Added html skeleton for blog home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <title>Gorilla Blog</title>
+</head>
+<body>
+  <h1 class="text-center mt-5 mb-4">Gorilla Blog</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-auto-md">
+        <div class="card border-0">
+          <nav id="selectCategory" class="nav flex-column">
+            <a id="ALL" class="nav-link category-menu my-1 active">All</a>
+          </nav>
+        </div>
+      </div>
+      <div class="col" id="posts"></div>
+    </div>
+  </div>
+</body>
+</html>
