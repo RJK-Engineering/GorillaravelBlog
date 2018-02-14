52a6878566dec1b7c035d34d2fcdeeefed750279
heinerbehrends <heinerbehrends@hotmail.com>
2018-02-15 00:10:30 +0100

    get('/post/create) does not work with get('/post/{post})

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 Route::get('/', 'PostController@index');
-Route::get('/post/{post}', 'PostController@show');
+// Route::get('/post/{post}', 'PostController@show');
 Route::get('/post/create', 'PostController@create');
 Route::post('/post', 'PostController@store');
-
+//
 Route::post('/comment/{post}', 'CommentController@store');
