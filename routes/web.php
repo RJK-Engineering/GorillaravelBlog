9c808472efb5afe9694541b37eab5b92b35563d2
RJK-Engineering <rjklinkhamer@gmail.com>
2018-02-14 12:01:28 +0100

    Added category routing

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,8 @@ Route::get('/', 'PostController@index');
 // Route::get('/{post}', 'PostController@show');
 Route::get('/create', 'PostController@create');
 Route::post('/posts', 'PostController@store');
+
+Route::get('/category', 'CategoryController@index');
+Route::get('/category/{category}', 'CategoryController@show');
+Route::get('/category/create', 'CategoryController@create');
+Route::post('/category', 'CategoryController@store');
