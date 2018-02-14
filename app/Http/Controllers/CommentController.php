ae7d2577dc0ee528b98c0e15fce017b6eab2df72
heinerbehrends <heinerbehrends@hotmail.com>
2018-02-14 23:09:44 +0100

    Added (Post $post) as arguments to store. First working version.

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,17 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
+use App\Comment;
 
 class CommentController extends Controller
 {
-    public function create() {
-        return view('comments.create');
+    public function create(Post $post) {
+        
+        return view('comments.create', compact('post'));
     }
 
-    public function store() {
+    public function store(Post $post) {
         Comment::create([
-            'comment' => request('comment');
-            'article_id' => request('article_id');
-        ])
+            'comment' => request('comment'),
+            'post_id' => $post->id,
+        ]);
+
+        return redirect('/post/' . $post->id);
     }
 }
