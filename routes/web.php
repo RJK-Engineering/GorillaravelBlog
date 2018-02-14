503477389be550bd7e69e98e6f3f118f0f5f274e
heinerbehrends <heinerbehrends@hotmail.com>
2018-02-14 23:22:35 +0100

    Changed comment post method to /comment/{post}. Removed unneccesary comments route.

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
-// use App\Post;
-
 Route::get('/', 'PostController@index');
-// Route::get('/{post}', 'PostController@show');
+Route::get('/post/{post}', 'PostController@show');
 Route::get('/post/create', 'PostController@create');
 Route::post('/post', 'PostController@store');
 
-Route::get('/comment/create', 'CommentController@create');
-Route::post('/comment'. 'CommentController@store');
+Route::post('/comment/{post}', 'CommentController@store');
