b5d22a4b1d96aeef043a60913d3e4d23ab59bf0e
heinerbehrends <heinerbehrends@hotmail.com>
2018-02-14 23:16:42 +0100

    added errors view

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,28 +1,31 @@
 @extends('master')
 
 @section('content')
-  <h1 class="mt-5 mb-4">New blog post</h1>
-  <form method="post" action="/posts" class="mb-5" >
+    <h1 class="mt-5 mb-4">New blog post</h1>
+    <form method="post" action="/post" class="mb-5" >
 
-    {{ csrf_field() }}
+        {{ csrf_field() }}
 
-  <div class="form-group">
-    <label for="categoriesSelector">Select a category. Select multiple categories by holding the ctrl key and clicking.</label>
-    <select required class="form-control" id="categoriesSelector" name="category">
-      <option value="1">Uncategorizable</option>
+        <div class="form-group">
+            <label for="categoriesSelector">Select a category. Select multiple categories by holding the ctrl key and clicking.</label>
+            <select class="form-control" id="categoriesSelector" name="category">
+                <option value="1">Uncategorizable</option>
 
-      @yield('categories')
+                @yield('categories')
 
-    </select>
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Title</label>
-    <input required class="form-control" id="exampleFormControlInput1" placeholder="Enter the title here" name="title">
-  </div>
-  <div class="form-group">
-    <label for="text-area">Write your blog post here</label>
-    <textarea required class="form-control" id="text-area" rows="10" placeholder="Enter the blogpost here" name="body"></textarea>
-  </div>
-  <input type="submit" value="Submit blog post" class="btn"></input>
-</form>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlInput1">Title</label>
+            <input class="form-control" id="exampleFormControlInput1" placeholder="Enter the title here" name="title">
+        </div>
+        <div class="form-group">
+            <label for="text-area">Write your blog post here</label>
+            <textarea class="form-control" id="text-area" rows="10" placeholder="Enter the blogpost here" name="body"></textarea>
+        </div>
+        <input type="submit" value="Submit blog post" class="btn"></input>
+    </form>
+
+    @include('errors')
+
 @endsection
