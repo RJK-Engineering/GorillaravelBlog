52b27018ae0f8ab8c2ec83d309565bebece53915
heinerbehrends <heinerbehrends@hotmail.com>
2018-02-15 14:22:10 +0100

    Added /category/{category}/posts route. Changed category/create to categories/create.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 Route::get('/', 'PostController@index');
+Route::get('/posts', 'PostController@index');
 Route::get('/posts/{post}', 'PostController@show');
+Route::get('/posts/category/{category}', 'PostController@show');
 Route::get('/post/create', 'PostController@create');
 Route::post('/post', 'PostController@store');
 
@@ -9,11 +11,14 @@ Route::post('/comment/{post}', 'CommentController@store');
 
 Route::get('/categories', 'CategoryController@index');
 // Route::get('/category/{category}', 'CategoryController@show');
-Route::get('/category/create', 'CategoryController@create');
+Route::get('/category/{category}/posts', 'CategoryController@getPosts');
+Route::get('/categories/create', 'CategoryController@create');
 Route::post('/category', 'CategoryController@store');
 
 Route::get('/comment/create', 'CommentController@create');
 Route::post('/comment'. 'CommentController@store');
 
-?>
+// Route::resource('comment', 'CommentController');
+// listen met artisan
 
+?>
