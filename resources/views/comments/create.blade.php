1cd55a8f343a517949978eb367c9d7d533833c2c
heinerbehrends <heinerbehrends@hotmail.com>
2018-02-16 13:37:42 +0100

    Changed label text

--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -1,7 +1,7 @@
-<form method="post" action="/comment/{{ $post->id }}" class="mb-5">
+<form method="post" action="/comment/{{ $post->id }}" class="mb-5 ml-auto mr-auto">
     {{ csrf_field() }}
-    <div class="form-group">
-        <label class="sr-only" for="exampleFormControlInput1">Title</label>
+    <div class="form-group pl-5">
+        <label class="sr-only" for="exampleFormControlInput1">Enter comment</label>
         <input required class="form-control" id="exampleFormControlInput1" placeholder="Enter your comment here" name="comment">
     </div>
 </form>
