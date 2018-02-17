989b7db416b31337850cbe2db6f9a22da01c1a68
heinerbehrends <heinerbehrends@hotmail.com>
2018-02-17 02:07:35 +0100

    Changed delete button to icon

--- resources/views/comments/show.blade.php
+++ resources/views/comments/show.blade.php
@@ -1,5 +1,5 @@
 <h3 id="comments" class="text-center pt-3 pb-2">Comments</h3>
-<ul class="list-group-flush pb-5">
+<ul class="list-group-flush mb-3">
     @foreach ($comments as $comment)
         <li class="justify-content-between list-group-item ">
             <div class="row">
@@ -10,8 +10,9 @@
                     <form class="form-inline" action="/comment/{{ $comment->id }}" method="POST">
                         {{ csrf_field() }}
                         {{ method_field('DELETE') }}
-
-                        <button class="btn btn-danger btn-small">Delete</button>
+                        <input type="submit"
+                        value="delete" style="border: none; background:none"
+                        class="material-icons"></input>
                     </form>
                 </div>
             </div>
