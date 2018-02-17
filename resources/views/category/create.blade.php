bc1c359a1cff7cc12a6bc87484ac4f36f260f521
heinerbehrends <heinerbehrends@hotmail.com>
2018-02-17 02:08:49 +0100

    Changed label to sr-only, changed submit button to icon

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -3,16 +3,19 @@
 @section('content')
 
     <h1 class="mt-5 mb-4">New Category</h1>
-    <form method="post" action="/category" class="mb-5" >
+    <form class="form-inline" method="post" action="/category" class="mb-5" >
 
         {{ csrf_field() }}
 
         <div class="form-group">
-            <label for="exampleFormControlInput1">Title</label>
-            <input required class="form-control" id="exampleFormControlInput1" placeholder="Enter category name here" name="title">
+            <label class="sr-only" for="exampleFormControlInput1">submit new category</label>
+            <input required class="form-control" id="exampleFormControlInput1"
+            placeholder="Enter category name here" name="title">
         </div>
 
-        <input type="submit" value="Submit" class="btn"></input>
+        <input type="submit" value="add_circle_outline"
+        class="material-icons ml-3" style="border: none; background:none; font-size: 36px">
+        </input>
     </form>
 
 @endsection
