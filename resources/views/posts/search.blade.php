d7cbbb633dd397d2b9ad2d731323fac35d6a6b9e
heinerbehrends <heinerbehrends@hotmail.com>
2018-02-20 00:46:15 +0100

    Changed method to post because of MethodNotAllowed error with get request

--- resources/views/posts/search.blade.php
+++ resources/views/posts/search.blade.php
@@ -1,6 +1,9 @@
-<form action="/posts/search" method="get">
+<form action="/posts/search" method="post">
+
+    {{ csrf_field() }}
+
     <div class="form-group">
-      <label for="search-box" class="sr-only">Search</label>
-      <input id="search-box" class="form-control" type="search" value="Search the blog">
+      <label class="sr-only" for="search-box" >Search</label>
+      <input class="form-control" id="search-box" name="q" type="text" value="Search the blog" >
     </div>
 </form>
