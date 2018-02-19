249aac81c6c217463bc660b91863ad45f43ae864
heinerbehrends <heinerbehrends@hotmail.com>
2018-02-19 22:41:55 +0100

    Basic search box

---
+++ resources/views/posts/search.blade.php
@@ -0,0 +1,6 @@
+<form action="/posts/search" method="get">
+    <div class="form-group">
+      <label for="search-box" class="sr-only">Search</label>
+      <input id="search-box" class="form-control" type="search" value="Search the blog">
+    </div>
+</form>
