3086721530c50b959bbf1bc1ee0d02ef09ad6e43
RJK-Engineering <rjklinkhamer@gmail.com>
2018-02-20 17:49:09 +0100

    Unsafely display body

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,7 +6,7 @@
       @if ($post->post_thumbnail)
           <img class="float-right mr-3" style="width: 25vw" src="/uploads/{{ $post->post_thumbnail }}">
       @endif
-      <p>{{ $post->body }}</p>
+      <p>{!! $post->body !!}</p>
     </article>
     @include('comments.show')
     @include('comments.on_off')
