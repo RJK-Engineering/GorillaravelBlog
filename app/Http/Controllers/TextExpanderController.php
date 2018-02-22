f9bef06a37bf2bdf9387ab11799151c0ceb3a581
heinerbehrends <heinerbehrends@hotmail.com>
2018-02-22 15:50:41 +0100

    TextExpanderController first commit. Implemented index(), json() and store() methods.

---
+++ app/Http/Controllers/TextExpanderController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TextExpander;
+use Illuminate\Http\Request;
+
+class TextExpanderController extends Controller
+{
+
+    public function index()
+    {
+        $snippets = TextExpander::get();
+        return view('textExpander.index', compact('snippets'));
+    }
+
+    public function json() {
+        return TextExpander::get();
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate(request(), [
+            'snippet' => 'required',
+            'abbreviation' => 'required',
+        ]);
+        $snippet = TextExpander::create([
+            'snippet' => request('snippet'),
+            'abbreviation' => request('abbreviation'),
+        ]);
+        return redirect('/posts/create');
+    }
+}
