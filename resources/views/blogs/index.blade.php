f9beabb6a03d2a48f60df17a319300b567b04026
RJK-Engineering <rjklinkhamer@gmail.com>
2018-02-23 11:52:47 +0100

    Reconfigured routing

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -3,17 +3,19 @@
 @section('content')
   <table class="table">
   <tr>
+    <th>Title</th>
     <th>Blogger</th>
     <th>Email</th>
-    <th>Title</th>
     <th>Edit</th>
     <th>Delete</th>
   </tr>
   @foreach ($blogs as $blog)
     <tr>
+      <td>
+        <a href="/{{ $blog->title }}">{{ $blog->title }}</a>
+      </td>
       <td>{{ $blog->user()->name }}</td>
       <td>{{ $blog->user()->email }}</td>
-      <td>{{ $blog->title }}</td>
       <td>
         <a class="material-icons" href="/blogs/{{ $blog->id }}/edit">edit</a>
       </td>
@@ -23,7 +25,7 @@
           {{ method_field('DELETE') }}
           <input class="material-icons"
                   type="submit" value="delete"
-                 style="border:none; background:none; cursor:pointer;">
+                 style="border:none; background:none; cursor:pointer; color:red">
         </form>
       </td>
     </tr>
