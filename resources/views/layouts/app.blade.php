99f31b2520eb212a83078848742c6812921927d9
heinerbehrends <heinerbehrends@hotmail.com>
2018-02-23 11:30:02 +0100

    layouts.app: added script categoryPosts.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -65,5 +65,6 @@
 
   <script src="{{ asset('js/app.js') }}"></script>
   <script src="{{ asset('js/textExpander.js') }}"></script>
+  <script src="{{ asset('js/categoryPosts.js') }}"></script>
 </body>
 </html>
