34a0b0a81535b07c8c22d53a6d3f1de51178b111
heinerbehrends <heinerbehrends@hotmail.com>
2018-02-23 11:29:38 +0100

    category.index: added class category-link to category nav-links

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,4 +1,4 @@
 
   @foreach ($categories as $category)
-    <a href="/categories/{{ $category->id }}/posts" class="nav-link">{{ $category->title }}</a>
+    <a href="/categories/{{ $category->id }}/posts" class="nav-link category-link">{{ $category->title }}</a>
   @endforeach
