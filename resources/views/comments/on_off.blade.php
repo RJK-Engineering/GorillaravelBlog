396efd25fb05f16905b47c6ed2208fe81e5c4c09
Rob Klinkhamer <rjklinkhamer@gmail.com>
2018-02-27 14:48:20 +0100

    Added @cans

--- resources/views/comments/on_off.blade.php
+++ resources/views/comments/on_off.blade.php
@@ -1,10 +1,12 @@
-<form class="form-inline" action="/posts/{{ $post->id }}/toggleCommentStatus" method="POST">
+@can('lock_comments')
+  <form class="form-inline" action="/posts/{{ $post->id }}/toggleCommentStatus" method="POST">
     {{ csrf_field() }}
     <button class="btn ml-auto mr-auto mb-3" style="cursor: pointer">
-        @if ($post->comments_on_off)
-            <i class="material-icons">lock_outline</i>
-        @else
-            <i class="material-icons">lock_open</i>
-        @endif
+      @if ($post->comments_on_off)
+        <i class="material-icons">lock_outline</i>
+      @else
+        <i class="material-icons">lock_open</i>
+      @endif
     </button>
-</form>
+  </form>
+@endcan
