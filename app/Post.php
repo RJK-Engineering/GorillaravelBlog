9616751403a97325af8149445aba15e279c05be5
Rob Klinkhamer <rjklinkhamer@gmail.com>
2018-02-27 12:26:27 +0100

    Removed search stuff

--- app/Post.php
+++ app/Post.php
@@ -1,27 +1,16 @@
 <?php
 
 namespace App;
-use Laravel\Scout\Searchable;
 use Carbon\Carbon;
 
 class Post extends Model
 {
-    use Searchable;
-
     public function comments() {
         return $this->hasMany(Comment::class);
     }
 
     public function categories() {
         return $this->belongsToMany(Category::class, 'post_categories');
-    }
-
-    public function toSearchableArray() {
-        $array = $this->toArray();
-
-        // Customize array...
-
-        return $array;
     }
 
     public function toggleCommentStatus() {
