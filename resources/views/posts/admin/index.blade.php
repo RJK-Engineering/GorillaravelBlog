e677af94fa058f03f4fc26a36c5ee85ad91ccbdc
Rob Klinkhamer <rjklinkhamer@gmail.com>
2018-02-27 12:20:36 +0100

    Squashed commit of the following:
    commit 19064d4beaf0237a4105d5ddebf8c91a6c1b037b
    Author: Rob Klinkhamer <rjklinkhamer@gmail.com>
    Date:   Tue Feb 27 12:15:30 2018 +0100
    
        Fix: name column should be in blog index
    
    commit b8577f37d937d5723a0655d49dc367242f37c529
    Author: Rob Klinkhamer <rjklinkhamer@gmail.com>
    Date:   Mon Feb 26 17:13:01 2018 +0100
    
        Added toggle comment status [W6-012]
    
    commit 5738455a91cd4c7bf481f871339f1a12f1f5d779
    Author: Rob Klinkhamer <rjklinkhamer@gmail.com>
    Date:   Mon Feb 26 17:02:42 2018 +0100
    
        Quick fix: db functions for postgres

--- resources/views/posts/admin/index.blade.php
+++ resources/views/posts/admin/index.blade.php
@@ -4,13 +4,26 @@
   <table class="table">
   <tr>
     <th>Title</th>
+    <th>Comments</th>
     <th>Edit</th>
     <th>Delete</th>
   </tr>
   @foreach ($posts as $post)
     <tr>
       <td>
-        <a href="/{{ $post->title }}">{{ $post->title }}</a>
+        {{ $post->title }}
+      </td>
+      <td>
+        <form class="form-inline" action="/posts/{{ $post->id }}/toggleCommentStatus" method="POST">
+          {{ csrf_field() }}
+          <button class="btn ml-auto mr-auto mb-3" style="cursor: pointer">
+            @if ($post->comments_on_off)
+              <i class="material-icons">lock_outline</i>
+            @else
+              <i class="material-icons">lock_open</i>
+            @endif
+          </button>
+        </form>
       </td>
       <td>
         <a class="material-icons" href="/posts/{{ $post->id }}/edit">edit</a>
