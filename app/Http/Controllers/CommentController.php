bb3f42c29fe4dc0e8b787e2c69bbcd1198c0880f
Rob Klinkhamer <rjklinkhamer@gmail.com>
2018-03-01 11:30:59 +0100

    Send mail upon new comment

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,38 +2,55 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Post;
 use App\Comment;
+use App\Post;
+use App\Mail\NewCommentMail;
+
+use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    public function __construct() {
-    $this->middleware('auth', ['except' => 'show']);
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => 'show']);
     }
 
-    public function index(Comment $comment) {
+    public function index(Comment $comment)
+    {
         return redirect('/posts/' . $comment->post_id . '#comments');
     }
 
-    public function show(Comment $comment) {
+    public function show(Comment $comment)
+    {
         return redirect('/posts/' . $comment->post_id . '#comments');
     }
 
-    public function create() {
+    public function create()
+    {
         return view('comments.create');
     }
 
-    public function store() {
+    public function store()
+    {
+        $postId = request('post_id');
         $comment = Comment::create([
             'comment' => request('comment'),
-            'post_id' => request('post_id')
+            'post_id' => $postId,
         ]);
+
+        $mail = new NewCommentMail($comment);
+        $blogger = Post::find($postId)->blogger();
+        \Mail::to($blogger)->send($mail);
+
         return back();
     }
 
-    public function destroy(Comment $comment) {
+    public function destroy(Comment $comment)
+    {
         $comment->delete();
         return back();
     }
+
 }
+
+?>
---
+++ resources/views/emails/new_comment.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to GorillaravelBlog</title>
+</head>
+<body>
+    <h1>New Comment</h1>
+    <p>Hi {{ $blogger->name }}, the following comment was made:</p>
+    <p>{{ $comment->comment }}</p>
+
+</body>
+</html>
