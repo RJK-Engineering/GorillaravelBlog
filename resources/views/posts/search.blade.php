8b5c307be1b82c9ce3c410cb463597fe57f2bdc6
heinerbehrends <heinerbehrends@hotmail.com>
2018-03-06 13:15:02 +0100

    search.blade: Added hidden input that submits blog_id

--- resources/views/posts/search.blade.php
+++ resources/views/posts/search.blade.php
@@ -1,6 +1,7 @@
-<form action="/posts/search" method="get">
+<form action="/{{ $blog->title }}/posts/search" method="get">
     <div class="form-group">
       <label class="sr-only" for="search-box">Search posts</label>
+      <input type="hidden" id="blog" name="blog" value="{{ $blog->id }}">
       <input class="form-control" id="search-box" size="16" name="q" type="text" placeholder="Search posts">
     </div>
 </form>
