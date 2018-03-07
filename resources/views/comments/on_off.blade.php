0e7400cb54da7b7467904c817e6c1c50094ca79f
heinerbehrends <heinerbehrends@hotmail.com>
2018-03-07 15:44:50 +0100

    comments.on_off: added {{ blog->title }} to url, added @if that checks if user owns post

--- resources/views/comments/on_off.blade.php
+++ resources/views/comments/on_off.blade.php
@@ -1,12 +1,15 @@
 @can('lock_comments')
-  <form class="form-inline" action="/posts/{{ $post->id }}/toggleCommentStatus" method="POST">
+  <form class="form-inline" action="/{{ $blog->title }}/posts/{{ $post->id }}/toggleCommentStatus" method="POST">
     {{ csrf_field() }}
-    <button class="btn ml-auto mr-auto mb-3" style="cursor: pointer">
+    @if(Auth::user()->can('lock_comments') && Auth::user()->id == $blog->user_id)
+      <button class="btn ml-auto mr-auto mb-3" style="cursor: pointer">
+
       @if ($post->comments_on_off)
         <i class="material-icons">lock_outline</i>
       @else
         <i class="material-icons">lock_open</i>
       @endif
     </button>
+    @endif
   </form>
 @endcan
